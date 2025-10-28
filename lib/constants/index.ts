bef092889dbe2e9d27b3c498188fb8f0d4311da7
Kuria-m <kuria@chpter.co>
2025-10-28 21:29:14 +0300

    supabase auth init

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -7,3 +7,16 @@
 export { default as dashboardSvgPaths } from './dashboard-svg-paths';
 export { default as websiteSvgPaths } from './website-svg-paths';
 
+// Route constants
+export {
+    PROTECTED_ROUTES,
+    AUTH_ROUTES,
+    PUBLIC_ROUTES,
+    AUTH_CALLBACK_ROUTES,
+    DEFAULT_PATHS,
+    isProtectedRoute,
+    isAuthRoute,
+    isPublicRoute,
+    isAuthCallbackRoute,
+} from './routes';
+
